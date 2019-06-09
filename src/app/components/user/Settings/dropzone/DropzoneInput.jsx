fb371e4be39b84be4fd96ec4ccbfd24488f0e21f
Vas <vasrokochuk@gmail.com>
2019-06-09 23:32:22 +0100

    Dropzone hook fix !!!! <- simple mistake

--- src/app/components/user/Settings/dropzone/DropzoneInput.jsx
+++ src/app/components/user/Settings/dropzone/DropzoneInput.jsx
@@ -3,13 +3,15 @@ import {useDropzone} from 'react-dropzone'
 import { Icon, Header } from 'semantic-ui-react';
 
 const DropzoneInput = ({ setFiles }) => {
+  
   const onDrop = useCallback(acceptedFiles => {
     setFiles(acceptedFiles);
   }, [setFiles]);
+
   const {getRootProps, getInputProps, isDragActive} = useDropzone({
     onDrop,
     multiple: true,
-    accept: 'iamge/*'
+    accept: 'image/*'
   })
 
   return (
