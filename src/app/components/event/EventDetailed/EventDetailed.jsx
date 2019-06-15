dbc7fa2087b9b13dba84474c58c4b433d35e12be
Vas <vasrokochuk@gmail.com>
2019-06-15 23:59:48 +0100

    SIngle events update

--- src/app/components/event/EventDetailed/EventDetailed.jsx
+++ src/app/components/event/EventDetailed/EventDetailed.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import EventDetailedHeader from './EventDetailedHeader';
 import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedSidebar from './EventDetailedSidebar';
+import { withFirestore } from 'react-redux-firebase';
 
 const mapState = (state, ownProps) => {
     const eventId = ownProps.match.params.id;
@@ -19,19 +20,29 @@ const mapState = (state, ownProps) => {
     }
 }
 
-const EventDetailed = ({event}) => {
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventDetailedHeader event={event} /> 
-                <EventDetailedInfo event={event}/>
-                <EventDetailedChat />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <EventDetailedSidebar attendees={event.attendees} />
-            </Grid.Column>
-        </Grid>
-    )
+class EventDetailed extends Component  {
+    async componentDidMount() {
+        const { firestore, match, history  }  = this.props;
+        let event = await firestore.get(`events/${match.params.id}`);
+        if (!event.exists) {
+            history.push('/events');
+        }
+    }
+    render() {
+        const {event} = this.props;
+        return (
+            <Grid>
+                <Grid.Column width={10}>
+                    <EventDetailedHeader event={event} /> 
+                    <EventDetailedInfo event={event}/>
+                    <EventDetailedChat />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <EventDetailedSidebar attendees={event.attendees} />
+                </Grid.Column>
+            </Grid>
+        )
+    }
 }
 
-export default connect(mapState)(EventDetailed);
+export default withFirestore(connect(mapState)(EventDetailed));
