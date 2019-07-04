4fd613dffaeb9bc5c2abcf5f2058494953e37afa
Vas <vasrokochuk@gmail.com>
2019-07-04 11:56:21 +0100

    Firebase Create Activity

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+exports.createActivity = functions.firestore.document('events/{eventId}').onCreate(event => {
+    let newEvent = event.data();
+
+    console.log(newEvent);
+
+    const activity = {
+        type: 'newEvent',
+        eventDate: newEvent.date,
+        hostedBy: newEvent.hostedBy,
+        title: newEvent.title,
+        photoURL: newEvent.hostPhotoUrl,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        hostUid: newEvent.hostUid,
+        eventId: event.id
+    }
+
+    console.log(activity);
+
+    return admin.firestore().collection('activity').add(activity).then((docRef) => {
+        return console.log('Activity cretea with ID: ', docRef.id)
+    }).catch((e) => {
+        return console.log('Error adding activity ', e);
+    })
+})
